0698f00dc7fdc121b55cbf34c1b34e283fe9aba3
Kevin Peery <kevin.peery@gmail.com>
2022-08-20 10:11:07 -0500

    fix coloring again

--- scriptHelper.js
+++ scriptHelper.js
@@ -99,7 +99,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     fuelStatus.innerHTML = "Fuel level high enough for launch";
     cargoStatus.innerHTML = "Cargo mass low enough for launch";
     launchStatus.innerHTML = "Shuttle is ready for launch";
-    launchStatus.style.color = "rgb(199, 37, 78)";
+    launchStatus.style.color = "rgb(65, 159, 106)";
   }
 }
 
